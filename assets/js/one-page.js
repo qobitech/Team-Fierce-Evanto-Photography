0aca145fe0a4d6b390db7a5a32e029a1bcc2e051
qobitech <qobitek@gmail.com>
2020-06-21 07:36:21 +0100

    added wow.js for better effect

--- assets/js/one-page.js
+++
@@ -1,7 +0,0 @@
-var menubtn = document.getElementById('menu_btn');
-var menu = document.getElementById('desktopnav');
-
-menubtn.addEventListener('click',()=>{
-    menu.style.transform =  menu.getBoundingClientRect() .x < 0  && `translateX(0)`
-    menu.style.transform =  menu.getBoundingClientRect() .x === 0  && `translateX(-100%)`
-})
